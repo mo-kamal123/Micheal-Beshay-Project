b5015945228a5275f07341725fe255ea32f7ce35
mostafa <mk4086219@gmail.com>
2025-06-24 01:56:50 +0300

    fix bugs

--- src/hooks/auth-hooks/useRegisterQuery.js
+++ src/hooks/auth-hooks/useRegisterQuery.js
@@ -3,13 +3,14 @@ import { registerUser } from "../../services/auth-services/register-user"
 import { changeStorage } from "../../utils/change-local-storage";
 
 
+// Custom hook for user registration using React Query's useMutation
 export const useRegister = () => {
     return useMutation({
         mutationFn: registerUser,
         onSuccess: (data) => {
             // Handle successful registration, e.g., store user data or tokens
             console.log("Registration successful:", data);
-            changeStorage("token", JSON.stringify(data.token));
+            // changeStorage("token", JSON.stringify(data.token));
         },
     })
 }
