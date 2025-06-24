5825af883a9e7a9c5ce5c4b3db64e2fa92e64abd
mostafa <mk4086219@gmail.com>
2025-06-25 01:23:43 +0300

    fix bug

--- src/services/coaches-services/coaches-services.js
+++ src/services/coaches-services/coaches-services.js
@@ -1,22 +1,12 @@
-import axios from 'axios';
+import { api } from '../axios-global';
 
 export const coachesServices = {
   getCoaches: async () => {
-    try {
-      const response = await axios.get('coaches');
-      return response.data;
-    } catch (error) {
-      console.error('failed to get all coaches:', error);
-      throw error;
-    }
+    const response = await api.get('coaches');
+    return response.data;
   },
-  getCoachDetails: async (id) => {
-    try {
-        const response = await axios.get(`coach/${id}`)
-        return response.data        
-    } catch (error) {
-        console.error(`failed to get coach with id: ${id} `, error);
-        throw error
-    }
-  }
+  getCoachDetails: async id => {
+    const response = await api.get(`coach/${id}`);
+    return response.data;
+  },
 };
