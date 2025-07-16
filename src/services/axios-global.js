9cd0f025af53a8a407ef6f57cea0e0fd3442a85b
mostafa <mk4086219@gmail.com>
2025-07-17 01:46:21 +0300

    add base url at axios global

--- src/services/axios-global.js
+++ src/services/axios-global.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = "http://www.example.com";
 export const api = axios.create({
-  baseURL: 'http://www.example.com',
+  baseURL: 'https://stg.michaelbeshay.com',
   headers: {
     'Content-Type': 'application/json',
   },
